b025868d930e9864fbd24762a1d7d4e7cd7aa0c0
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 22:40:17 -0300

    feat: adição do arquivo header.module.ts

---
+++ developments/ld-app-8/src/app/components/header/header.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header.component';
+import { HeaderRoutingModule } from './header-routing.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HeaderRoutingModule
+  ],
+  exports: []
+})
+
+export class HeaderModule { }
